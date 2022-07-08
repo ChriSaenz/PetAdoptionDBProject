5ac98f2991a7eebe2b721a4ef236b394f27bd330
Chase Foster <1000075553@hexaware.com>
2022-07-08 17:13:55 -0400

    Added more stuff

--- Adoption_System.sql
+++ Adoption_System.sql
@@ -1,8 +1,19 @@
 -- -----------------------------------------------------
 -- Schema adoption_system
 -- -----------------------------------------------------
+
 -- drop database adoption_system;
--- drop table Customer cascade constraint;
+
+-- set foreign_key_checks = 0;
+-- drop table Customer;
+-- drop table Request;
+-- drop table Pet;
+-- drop table Employee;
+-- drop table Receipt;
+-- set foreign_key_checks = 1;
+
+
+-- drop table Customer cascade;
 -- drop table Request cascade;
 -- drop table Pet cascade;
 -- drop table Employee cascade;
@@ -20,7 +31,7 @@ USE `adoption_system` ;
 CREATE TABLE IF NOT EXISTS `adoption_system`.`Customer` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
-  `phoneNumber` VARCHAR(45) NULL,
+  `phone_number` VARCHAR(45) NULL,
   `date_joined` DATE NULL,
   `birthday` DATE NULL,
   PRIMARY KEY (`id`))
@@ -52,7 +63,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `adoption_system`.`Pet` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `request_id` INT NOT NULL,
+  `Request_id` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `age` INT NOT NULL,
   `species` VARCHAR(45) NOT NULL,
@@ -63,10 +74,10 @@ CREATE TABLE IF NOT EXISTS `adoption_system`.`Pet` (
   `vaccinated` TINYINT NOT NULL,
   `date_acquired` DATE NOT NULL,
   `cost` DOUBLE NULL,
-  PRIMARY KEY (`id`, `request_id`),
-  INDEX `fk_Pet_Request1_idx` (`request_id` ASC) VISIBLE,
+  PRIMARY KEY (`id`, `Request_id`),
+  INDEX `fk_Pet_Request1_idx` (`Request_id` ASC) VISIBLE,
   CONSTRAINT `fk_Pet_Request1`
-    FOREIGN KEY (`request_id`)
+    FOREIGN KEY (`Request_id`)
     REFERENCES `adoption_system`.`Request` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -78,11 +89,13 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `adoption_system`.`Employee` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `phone` VARCHAR(45) NOT NULL,
   `salary` DOUBLE NOT NULL,
   `title` VARCHAR(45) NOT NULL,
   `admin` TINYINT NULL,
-  `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -118,18 +131,50 @@ CREATE TABLE IF NOT EXISTS `adoption_system`.`Receipt` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
-
 -- ------------------
-INSERT INTO Customer (name, phoneNumber, date_joined, birthday)
+INSERT INTO Customer (name, phone_Number, date_joined, birthday)
 VALUES ('Chase', '123-456-7890', '2022-07-08', '1999-08-31'), 
 ("Diego", "234-567-8901", '2020-07-19', '2021-07-19'),
 ("Chris", "334-577-8905", '1990-07-19', '2022-07-19'),
 ("Felix", "234-567-8901", '2020-07-19', '2000-07-19');
 
-INSERT INTO Pet (requestID, requestCustomerID, name, age, species, breed, color, sex, neutered, vaccinated, date_acquired, cost)
-VALUES (1, 2, "Chase", 2, "Dog", "Cocker Spaniel", "Brown", "Male", 1, 1, '2022-05-20', 1500.01),
-(2, 3, "Phantom", 3, "Dog", "American Shorthair", "Gray", "Male", 1, 1, '2020-04-10', 2000.01);
- 
+insert into Employee(username, password, name, phone, salary, title, admin) values
+("Boss", "P42", "Biggs", "(555)", 500000.00, "Big Boss", 1),
+("Linked", "33", "Stack", "(505)", 80000.00, "Employee A", 0),
+("Inn", "P42", "Over", "(755)", 50000.00, "Employee B", 0),
+("Pause", "P42", "Flow", "(855)", 70000.00, "Employee C", 0),
+("Menu", "P42", "Thanks", "(655)", 100000.00, "Employee D", 0);
+-- Employee Works!
 
--- ------------------
+
+insert into Request(customer_id, pet_id, date, status, employee_id) values
+(2, 1, '2022-07-7', "Pending", 1),
+(2, 2, '2022-07-8', "Pending", 2),
+(2, 3, '2022-07-9', "Pending", 3),
+(1, 4, '2022-07-8', "Pending", 4),
+(3, 5, '2022-07-8', "Pending", 5);
+
+INSERT INTO Pet (request_id, name, age, species, breed, color, sex, neutered, vaccinated, date_acquired, cost)
+VALUES (1, "Chase", 2, "Dog", "Cocker Spaniel", "Brown", "Male", 1, 1, '2022-05-20', 1500.01),
+(2, "Phantom", 3, "Dog", "American Shorthair", "Gray", "Male", 1, 1, '2020-04-10', 2000.34),
+(3, "Ace", 1, "Dog", "Shiba", "Brown", "Male", 1, 1, '2022-07-15', 1250),
+(4, "Dexter", 5, "Cat", "Shorthair", "Tabby", "Male", 1, 1, '2017-03-18', 250);
+ 
+INSERT INTO Receipt (employee_id, customer_id, request_id, date, cost)
+VALUES (1, 1, 1, '2022-07-02', 1500.01),
+(2, 2, 2, '2021-06-03', 2000),
+(3, 3, 3, '2020-01-01', 1250);
+
+
+insert into Request(customer_id, pet_id, date, status, employee_id) values
+(2, 1, '2022-07-7', "Pending", 1),
+(2, 2, '2022-07-8', "Pending", 2),
+(2, 3, '2022-07-9', "Pending", 3),
+(1, 4, '2022-07-8', "Pending", 4),
+(3, 5, '2022-07-8', "Pending", 5);
+
+select * from Customer;
+select * from Request;
+select * from Pet;
+select * from Employee;
+select * from Receipt;
