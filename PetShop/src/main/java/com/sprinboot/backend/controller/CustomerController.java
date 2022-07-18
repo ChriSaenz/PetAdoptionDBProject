fd8fe05efb623eeb6e57ada1b75fefd43a15f101
Diego Cordova <1000075557@hexaware.com>
2022-07-18 17:53:31 -0400

    Customer and Employee added

---
+++ PetShop/src/main/java/com/sprinboot/backend/controller/CustomerController.java
@@ -0,0 +1,42 @@
+package com.sprinboot.backend.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sprinboot.backend.model.Customer;
+import com.sprinboot.backend.repository.CustomerRepository;
+
+@RestController
+public class CustomerController {
+	@Autowired
+	private CustomerRepository categoryRepository;
+
+	@PostMapping("/customer")
+	public void postCategory(@RequestBody Customer category)
+	{
+		categoryRepository.save(category);
+	}
+
+	@GetMapping("/customer")
+	public List<Customer> getAllCategories()
+	{
+		return categoryRepository.findAll();
+	}
+
+	@GetMapping("/customer/{id}")
+	public Customer getCustomerById(@PathVariable("id") Long id)
+	{
+		Optional<Customer> optional =  categoryRepository.findById(id);
+
+		if(optional.isPresent())
+			return optional.get();
+		throw new RuntimeException("ID is invalid");
+	}
+}
