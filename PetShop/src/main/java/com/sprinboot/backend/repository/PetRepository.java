45927c6bbf1f884ceea66000ab067267a17f128f
Diego Cordova <1000075557@hexaware.com>
2022-07-18 16:42:55 -0400

    PetAdoption v2

---
+++ PetShop/src/main/java/com/sprinboot/backend/repository/PetRepository.java
@@ -0,0 +1,5 @@
+package com.sprinboot.backend.repository;
+
+public class PetRepository {
+
+}
---
+++ Spring_SQL.sql
@@ -0,0 +1,9 @@
+use mavericks_75557v2;
+
+show tables;
+
+describe categories;
+select * from categories;
+
+describe vendor;
+select * from vendor;
