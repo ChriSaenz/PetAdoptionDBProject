ccd1afcca35699ffee08852f2ad831889d3392ab
Diego Cordova <1000075557@hexaware.com>
2022-07-19 17:09:24 -0400

    Few tasks of the day

--- PetShop/src/main/java/com/sprinboot/backend/controller/EmployeeController.java
+++ PetShop/src/main/java/com/sprinboot/backend/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,27 +17,33 @@ import com.sprinboot.backend.repository.EmployeeRepository;
 @RestController
 public class EmployeeController {
 	@Autowired
-	private EmployeeRepository categoryRepository;
+	private EmployeeRepository employeeRepository;
 
 	@PostMapping("/employee")
 	public void postCategory(@RequestBody Employee category)
 	{
-		categoryRepository.save(category);
+		employeeRepository.save(category);
 	}
 
 	@GetMapping("/employee")
-	public List<Employee> getAllCategories()
+	public List<Employee> getAllEmployees()
 	{
-		return categoryRepository.findAll();
+		return employeeRepository.findAll();
 	}
 
 	@GetMapping("/employee/{id}")
 	public Employee getEmployeeById(@PathVariable("id") Long id)
 	{
-		Optional<Employee> optional =  categoryRepository.findById(id);
+		Optional<Employee> optional =  employeeRepository.findById(id);
 
 		if(optional.isPresent())
 			return optional.get();
 		throw new RuntimeException("ID is invalid");
+	}
+	
+	@DeleteMapping("/employee/{id}")
+	public void deleteEmployeeById(@PathVariable("id")Long id)
+	{
+		employeeRepository.deleteById(id);
 	}
 }
